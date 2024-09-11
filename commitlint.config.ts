1e9a186890cb01c3d81998a8a1e939a5cad05bca
Vijay Singh <dprophecyguy@gmail.com>
2024-09-11 12:23:13 +0530

    feat: setup commitlint and lintstaged

---
+++ commitlint.config.ts
@@ -0,0 +1,19 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-case": [2, "always", "lower-case"],
+  },
+};
+
+export default Configuration;
